a527cac5ca117aa39bfa8918a9bcd1e0cd28c8b3
Zeeson <olabrazanislam@gmail.com>
2021-03-06 22:47:48 +0100

    reinsall modules

--- components/dashboard/access/product.js
+++ components/dashboard/access/product.js
@@ -23,7 +23,6 @@ function product({ product, handleApply }) {
         <p className='price'>N {product.product_price}</p>
       </div>
       <div className="btn_holder">
-      {/* <Link href="/success"> */}
       <button
               className="btn btn-primary"
               disabled={snapshot.userInfo?.payment_due !== 0}
@@ -31,7 +30,6 @@ function product({ product, handleApply }) {
             >
               Request
             </button>
-        {/* </Link> */}
             </div>
     </div>
   );
